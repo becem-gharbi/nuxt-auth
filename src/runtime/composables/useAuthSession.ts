3ed605b721ef522356655ce6bb30696b23ef399c
becem-gharbi <99251251+becem-gharbi@users.noreply.github.com>
2023-01-30 16:07:58 +0100

    fix: uaParser not found

--- src/runtime/composables/useAuthSession.ts
+++ src/runtime/composables/useAuthSession.ts
@@ -9,7 +9,7 @@ import {
   useRequestEvent,
   useRequestHeaders,
 } from "#app";
-import { UAParser } from "ua-parser-js";
+import uaParserJs from "ua-parser-js";
 
 export default function () {
   const privateConfig = useRuntimeConfig().auth;
@@ -131,15 +131,15 @@ export default function () {
     });
 
     const sessions: Session[] = refreshTokens.map((refreshToken) => {
-      const uaParser = new UAParser(refreshToken.userAgent || undefined);
+      const ua = uaParserJs(refreshToken.userAgent || undefined);
 
       return {
         id: refreshToken.id,
         active: refreshToken.id === active,
         userId: refreshToken.userId,
-        browser: uaParser.getBrowser(),
-        device: uaParser.getDevice(),
-        os: uaParser.getOS(),
+        browser: ua.browser,
+        device: ua.device,
+        os: ua.os,
         updatedAt: refreshToken.updatedAt,
         createdAt: refreshToken.createdAt,
       };
