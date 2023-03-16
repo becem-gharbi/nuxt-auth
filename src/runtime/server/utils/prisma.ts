475f7318464511415680e42a4ea8bd5884a9a24a
becem-gharbi <99251251+becem-gharbi@users.noreply.github.com>
2023-03-16 12:31:57 +0100

    fix: explicitly set exported prisma instance type

--- src/runtime/server/utils/prisma.ts
+++ src/runtime/server/utils/prisma.ts
@@ -5,7 +5,8 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const prisma = globalThis.prisma || new PrismaClient(privateConfig.prisma);
+const prisma: PrismaClient =
+  globalThis.prisma || new PrismaClient(privateConfig.prisma);
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
 
 export { prisma };
