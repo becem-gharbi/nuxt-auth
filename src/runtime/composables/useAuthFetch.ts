a96e47155e4345bcb01faedb804c1dbcaa545731
becem gharbi <becem.gharbi@live.com>
2023-08-13 14:07:50 +0100

    fix(useAuthFetch): Remove logout when access token not found

--- src/runtime/composables/useAuthFetch.ts
+++ src/runtime/composables/useAuthFetch.ts
@@ -1,5 +1,4 @@
 import useAuthSession from "./useAuthSession";
-import useAuth from "./useAuth";
 import { defu } from "defu";
 import type { NitroFetchRequest, NitroFetchOptions } from "nitropack";
 
@@ -12,26 +11,21 @@ export default async function <DataT>(
 ): Promise<DataT> {
   const { getAccessToken } = useAuthSession();
 
-  const { logout } = useAuth();
+  // Remove refresh token from request
+  options.credentials = options.credentials || "omit";
 
   // Get a freshed access token (refreshed if expired) or not
   const accessToken = await getAccessToken();
 
   // If the access token cannot be obtained, proceed to logout!
-  if (!accessToken) {
-    await logout();
-    throw new Error("unauthorized");
+  if (accessToken) {
+    options.headers = defu(
+      {
+        Authorization: "Bearer " + accessToken,
+      },
+      options.headers
+    );
   }
-
-  // Remove refresh token from request
-  options.credentials = options.credentials || "omit";
-
-  options.headers = defu(
-    {
-      Authorization: "Bearer " + accessToken,
-    },
-    options.headers
-  );
 
   return $fetch(request, options);
 }
