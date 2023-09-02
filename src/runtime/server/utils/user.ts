70cacfcdff54c09dec8fab09484d237f72f21f64
becem gharbi <becem.gharbi@live.com>
2023-09-02 14:59:52 +0100

    refactor(generateAvatar): Select background color from predefined colors

--- src/runtime/server/utils/user.ts
+++ src/runtime/server/utils/user.ts
@@ -42,12 +42,32 @@ export async function createUser(
 }
 
 function generateAvatar(name: string) {
+  // https://tailwindcss.com/docs/customizing-colors#default-color-palette
+  // Variant 700
+  const colors = [
+    "b91c1c" /*red*/,
+    "c2410c" /*orange*/,
+    "b45309" /*amber*/,
+    "a16207" /*yellow*/,
+    "4d7c0f" /*lime*/,
+    "15803d" /*green*/,
+    "0f766e" /*teal*/,
+    "0e7490" /*cyan*/,
+    "1d4ed8" /*blue*/,
+    "4338ca" /*indigo*/,
+    "6d28d9" /*violet*/,
+    "be123c" /*rose*/,
+  ];
+
+  const randomIndex = Math.floor(Math.random() * (colors.length - 1));
+
   const url = withQuery("https://ui-avatars.com/api", {
     name: name,
-    background: "random",
+    color: "f5f5f5",
+    background: colors[randomIndex],
     length: 1,
     bold: true,
-    "font-size": 0.6,
+    "font-size": 0.5,
   });
 
   return url;
