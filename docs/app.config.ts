24b2929ddd5125ba7d906ef1a1f336b00174e7f0
becem-gharbi <becem.gharbi@live.com>
2023-10-15 06:08:01 +0100

    docs: Change social card

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -4,7 +4,7 @@ export default defineAppConfig({
     description:
       "A fairly complete solution to handle authentication for your Nuxt 3 project",
     image:
-      "https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png",
+      "/cover.jpg",
     socials: {
       //  twitter: "nuxt_js",
       github: "becem-gharbi/nuxt-auth",
