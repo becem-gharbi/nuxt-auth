7dc51c62a54bc682bbbd173da87539ad09e11cc6
becem-gharbi <becem.gharbi@live.com>
2023-11-24 15:44:56 +0100

    refactor(fingerprint): Use h3 built-in hash option

--- src/runtime/server/utils/token/fingerprint.ts
+++ src/runtime/server/utils/token/fingerprint.ts
@@ -1,28 +1,20 @@
 import { getRequestFingerprint } from 'h3'
 import type { H3Event } from 'h3'
-import { hashSync, compareSync } from '../bcrypt'
 
-async function getFingerprint (event: H3Event) {
+export async function getFingerprintHash (event:H3Event) {
   const fingerprint = await getRequestFingerprint(event, {
     ip: false,
     userAgent: true,
-    hash: false
+    hash: 'SHA-1'
   })
 
   return fingerprint ?? ''
 }
 
-export async function getFingerprintHash (event:H3Event) {
-  const fingerprint = await getFingerprint(event)
-
-  return fingerprint && hashSync(fingerprint, 12)
-}
-
 export async function verifyFingerprint (event: H3Event, hash:string) {
-  const fingerprint = await getFingerprint(event)
-  const match = compareSync(fingerprint, hash)
+  const fingerprintHash = await getFingerprintHash(event)
 
-  if (match === false) {
+  if (fingerprintHash !== hash) {
     throw new Error('unauthorized')
   }
 }
