e9b98e35819e6d9651e1a20bdb458781da3c0be8
becem-gharbi <becem.gharbi@live.com>
2023-11-24 10:48:28 +0100

    perf: Avoid access token check on non API requests

--- src/runtime/server/plugins/middleware.ts
+++ src/runtime/server/plugins/middleware.ts
@@ -1,8 +1,13 @@
+import type { H3Event } from 'h3'
 import { getAccessTokenFromHeader, verifyAccessToken } from '#auth'
 import { defineNitroPlugin } from '#imports'
 
 export default defineNitroPlugin((nitroApp) => {
-  nitroApp.hooks.hook('request', async (event) => {
+  nitroApp.hooks.hook('request', async (event: H3Event) => {
+    if (event.path.startsWith('/api') === false) {
+      return
+    }
+
     const accessToken = getAccessTokenFromHeader(event)
 
     if (accessToken) {
