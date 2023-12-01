15acf8bb6c3c14d5baabff177a6b9a0414903b65
becem-gharbi <becem.gharbi@live.com>
2023-12-01 11:32:18 +0100

    feat: Add support for prisma accelerate on edge runtimes

--- src/runtime/server/plugins/prisma.edge.ts
+++ src/runtime/server/plugins/prisma.edge.ts
@@ -1,11 +1,12 @@
 import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 import { getConfig } from '../utils'
 // eslint-disable-next-line import/named
 import { defineNitroPlugin } from '#imports'
 
 export default defineNitroPlugin((nitroApp) => {
   const config = getConfig()
-  const prisma = new PrismaClient(config.private.prisma)
+  const prisma = new PrismaClient(config.private.prisma).$extends(withAccelerate())
 
   nitroApp.hooks.hook('request', (event) => {
     event.context.prisma = prisma
