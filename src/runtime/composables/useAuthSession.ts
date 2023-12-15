8a8d5533af9a62ba5d00af1aae7c8e43d37d5356
Edan Weis <edanweis@gmail.com>
2023-12-15 23:01:04 +1100

    add: clear cookies

--- src/runtime/composables/useAuthSession.ts
+++ src/runtime/composables/useAuthSession.ts
@@ -56,6 +56,7 @@ export function useAuthSession () {
     clear: () => {
       if (process.server) {
         deleteCookie(event, accessTokenCookieName)
+        deleteCookie(event, providerTokenCookieName)
       } else {
         Cookies.remove(accessTokenCookieName)
       }
