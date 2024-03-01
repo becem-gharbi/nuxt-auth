e8d8103035f3f9c30852532b007404c71f4aebbd
becem-gharbi <becem.gharbi@live.com>
2024-03-01 15:59:25 +0100

    docs: change font family

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,15 +1,22 @@
 export default defineNuxtConfig({
-  // https://github.com/nuxt-themes/docus
   extends: '@nuxt-themes/docus',
-
-  modules: [
-    // https://github.com/nuxt-modules/plausible
-    '@nuxtjs/plausible',
-    // https://github.com/nuxt/devtools
-    '@nuxt/devtools'
-  ],
-
-  colorMode: {
-    preference: 'dark'
-  }
+  imports: {
+    autoImport: true
+  },
+  app: {
+    head: {
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: 'anonymous'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;500;600&display=swap'
+        }
+      ]
+    }
+  },
 })
