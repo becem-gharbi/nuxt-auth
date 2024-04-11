654332676dba6b7598a5754f1d960f1ce2a54ce0
becem-gharbi <becem.gharbi@live.com>
2024-04-11 07:48:36 +0100

    refactor: remove `useAuthFetch` deprecation

--- src/runtime/composables/useAuthFetch.ts
+++ src/runtime/composables/useAuthFetch.ts
@@ -1,27 +1,3 @@
-import { defu } from 'defu'
-import type { NitroFetchRequest, NitroFetchOptions } from 'nitropack'
-import type { PublicConfig } from '../types'
-import { useAuthSession, useRequestHeaders, useRuntimeConfig } from '#imports'
+import { useNuxtApp } from '#imports'
 
-/**
- * @deprecated since version 2.2.1, please use `useNuxtApp().$auth.fetch() instead`
- */
-export async function useAuthFetch <T> (
-  request: NitroFetchRequest,
-  options?: NitroFetchOptions<NitroFetchRequest>
-): Promise<T> {
-  const publicConfig = useRuntimeConfig().public.auth as PublicConfig
-  const reqHeaders = useRequestHeaders(['user-agent'])
-  const accessToken = await useAuthSession().getAccessToken()
-
-  const _options = defu(options, reqHeaders, {
-    credentials: 'omit',
-    headers: accessToken && {
-      authorization: 'Bearer ' + accessToken
-    }
-  }) as NitroFetchOptions<NitroFetchRequest>
-
-  _options.baseURL = publicConfig.backendBaseUrl
-
-  return $fetch<any>(request, _options)
-}
+export const useAuthFetch: typeof $fetch = useNuxtApp().$auth.fetch
