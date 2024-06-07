ec7664def9f2a01d912ffa25c53da5178943ad7c
Becem <becem.gharbi@live.com>
2024-06-07 19:06:53 +0100

    docs: update to v3 (#52)

--- playground/server/plugins/hooks.ts
+++ playground/server/plugins/hooks.ts
@@ -1,10 +1,21 @@
 import type { NitroApp } from 'nitropack'
 import consola from 'consola'
+import { defineAdapter, setEventContext } from '#auth_utils'
 
 // @ts-expect-error importing an internal module
 import { defineNitroPlugin } from '#imports'
 
+const useAdapter = defineAdapter<undefined>(() => {
+  return {
+
+  }
+})
+
 export default defineNitroPlugin((nitroApp: NitroApp) => {
+  const adapter = useAdapter(undefined)
+
+  nitroApp.hooks.hook('request', event => setEventContext(event, adapter))
+
   nitroApp.hooks.hook('auth:email', (from, message) => {
     consola.info('Email', from, message)
   })
