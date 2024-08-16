6be46b1c532b868aaa44b116a7cdfaf07796d10a
becem-gharbi <becem.gharbi@live.com>
2024-08-16 15:20:25 +0100

    fix: automatically revoke expired sessions

--- src/runtime/server/api/sessions/index.get.ts
+++ src/runtime/server/api/sessions/index.get.ts
@@ -1,5 +1,6 @@
 import { defineEventHandler } from 'h3'
-import { handleError, createUnauthorizedError } from '../../utils'
+import { handleError, createUnauthorizedError, getConfig } from '../../utils'
+import type { Session } from '#auth_adapter'
 
 export default defineEventHandler(async (event) => {
   try {
@@ -9,11 +10,22 @@ export default defineEventHandler(async (event) => {
       throw createUnauthorizedError()
     }
 
+    const config = getConfig()
     const sessions = await event.context.auth.adapter.session.findManyByUserId(authData.userId)
 
+    const expired: Session[] = []
+    const active: Session[] = []
+
+    sessions.forEach((session) => {
+      const isExpired = new Date().getTime() - new Date(session.updatedAt).getTime() > config.private.refreshToken.maxAge! * 1000
+      isExpired ? expired.push(session) : active.push(session)
+    })
+
+    await Promise.all(expired.map(session => event.context.auth.adapter.session.delete(session.id, authData.userId)))
+
     return {
-      active: sessions,
-      current: sessions.find(session => session.id === authData.sessionId),
+      active,
+      current: active.find(session => session.id === authData.sessionId),
     }
   }
   catch (error) {
