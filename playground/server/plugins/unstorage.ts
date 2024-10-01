f8458e35c91710b0b8011b76c342fde6776a4ad8
becem-gharbi <becem.gharbi@live.com>
2024-10-01 10:35:09 +0100

    chore(playground): use memory storage with unstorage

--- playground/server/plugins/unstorage.ts
+++ playground/server/plugins/unstorage.ts
@@ -1,6 +1,6 @@
 import type { NitroApp } from 'nitropack'
 import { createStorage } from 'unstorage'
-import fsDriver from 'unstorage/drivers/fs'
+// import fsDriver from 'unstorage/drivers/fs'
 import consola from 'consola'
 import { defineNitroPlugin } from 'nitropack/runtime'
 import { useUnstorageAdapter, setEventContext } from '#auth_utils'
@@ -9,9 +9,11 @@ export default defineNitroPlugin((nitroApp: NitroApp) => {
   if (process.env.NUXT_ADAPTER === 'unstorage') {
     consola.success('Running with Unstorage adapter')
 
-    const storage = createStorage({
-      driver: fsDriver({ base: './playground/unstorage_data' }),
-    })
+    // const storage = createStorage({
+    //   driver: fsDriver({ base: './playground/unstorage_data' }),
+    // })
+
+    const storage = createStorage()
 
     const adapter = useUnstorageAdapter(storage)
 
